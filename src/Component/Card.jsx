c88394cef51711f09862604e81d343eaedaa7811
4shaham <143824921+4shaham@users.noreply.github.com>
2025-02-10 20:50:52 +0530

    completed

---
+++ src/Component/Card.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+function Card() {
+  return (
+    <div className=" flex flex-col my-6 bg-white shadow-sm border border-slate-200 rounded-lg w-90 p-6">
+      <div className="mb-1 text-start">
+        <h5 className="text-black text-xl font-semibold">
+          Website Review Check
+        </h5>
+      </div>
+      <p className="block text-gray-600 leading-normal font-light mb-4">
+        <span className="text-red-400 font-bold">Focus pack</span> | 1 year
+      </p>
+      <p className="block text-slate-600 leading-normal font-light mb-4">
+        Because it&apos;s about motivating the doers. Because I&apos;m here to
+        follow my dreams and inspire others.
+      </p>
+      <div className="flex justify-between">
+        <p className="text-green-500 font-bold text-lg">1200</p>
+        <p className="text-end  text-blue-600 font-semibold text-lg hover:underline flex items-center">
+          View Details
+        </p>
+      </div>
+      
+    </div>
+
+  );
+}
+
+export default Card;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../Component/Navbar";
 import HeroPage from "../Component/HeroPage";
+import Card from "../Component/card";
 
 function HomePage() {
+  const [button, setButtonStatus] = useState([
+    { name: "Railway", status: true },
+    { name: "SSC", status: false },
+    { name: "Teaching", status: false },
+  ]);
+  const [currentButton, setCurrentButton] = useState("Railway");
+
+  const buttonRelatedData = [
+    { category: "Railway" },
+    { category: "Railway" },
+    { category: "Railway" },
+    { category: "Railway" },
+    { category: "Railway" },
+    { category: "Railway" },
+    { category: "SSC" },
+    { category: "SSC" },
+    { category: "SSC" },
+    { category: "SSC" },
+    { category: "SSC" },
+    { category: "Teaching" },
+  ];
+
   const onlineCourses = [
     {
       heading: "Complete Syllabus with Clear Course Design",
@@ -11,24 +34,38 @@ function HomePage() {
         "You can start with any section without order as live classes, videos, pdfs and tests are not only designed in a systematic path but also curated with flexibility.",
     },
     {
-        heading: "Systematic but Flexible Learning Path",
-        img: "https://www.examys.com/_next/image?url=%2Fstatic%2Fhome-2.png&w=384&q=75",
-        content:
-          "You can start with any section without order as live classes, videos, pdfs and tests are not only designed in a systematic path but also curated with flexibility.",
-      },
-      {
-        heading: "Complete Syllabus with Clear Course Design",
-        img: "https://www.examys.com/_next/image?url=%2Fstatic%2Fhome-1.png&w=384&q=75",
-        content:
-          "You can start with any section without order as live classes, videos, pdfs and tests are not only designed in a systematic path but also curated with flexibility.",
-      },
-      {
-        heading: "Complete Syllabus with Clear Course Design",
-        img: "https://www.examys.com/_next/image?url=%2Fstatic%2Fhome-1.png&w=384&q=75",
-        content:
-          "You can start with any section without order as live classes, videos, pdfs and tests are not only designed in a systematic path but also curated with flexibility.",
-      },
+      heading: "Systematic but Flexible Learning Path",
+      img: "https://www.examys.com/_next/image?url=%2Fstatic%2Fhome-2.png&w=384&q=75",
+      content:
+        "You can start with any section without order as live classes, videos, pdfs and tests are not only designed in a systematic path but also curated with flexibility.",
+    },
+    {
+      heading: "Complete Syllabus with Clear Course Design",
+      img: "https://www.examys.com/_next/image?url=%2Fstatic%2Fhome-1.png&w=384&q=75",
+      content:
+        "You can start with any section without order as live classes, videos, pdfs and tests are not only designed in a systematic path but also curated with flexibility.",
+    },
+    {
+      heading: "Complete Syllabus with Clear Course Design",
+      img: "https://www.examys.com/_next/image?url=%2Fstatic%2Fhome-1.png&w=384&q=75",
+      content:
+        "You can start with any section without order as live classes, videos, pdfs and tests are not only designed in a systematic path but also curated with flexibility.",
+    },
   ];
+
+  const handlButtonClickStatus = (name) => {
+    setButtonStatus(
+      button.filter((values) => {
+        if (values.name == name) {
+          values.status = true;
+        } else {
+          values.status = false;
+        }
+        return values;
+      })
+    );
+    setCurrentButton(name);
+  };
 
   return (
     <>
@@ -36,7 +73,6 @@ function HomePage() {
       <HeroPage />
 
       <div className="w-full  bg-gray-50 items-center p-4">
-
         <div className="items-center mx-auto text-center mt-10 font-semibold">
           <h1 className="text-5xl">Exmays Course Advantages</h1>
           <h1 className="text-xl mt-3 text-gray-400">
@@ -44,7 +80,6 @@ function HomePage() {
             classes, pdfs and quizzes
           </h1>
           <div className="container mx-auto grid grid-cols-4 gap-5 mt-7  p-4">
-
             {onlineCourses.map((values) => (
               <div className="w-70">
                 <img src={values.img} alt="" className="object-fill" />
@@ -52,6 +87,39 @@ function HomePage() {
                 <small className="text-gray-400 mt-3">{values.content}</small>
               </div>
             ))}
+          </div>
+
+          <div className="text-start mt-7 mx-17">
+            <h1 className="text-3xl font-bold">Some Courses You May Like</h1>
+            <div className="flex gap-5 mt-5">
+              {button.map((values) => (
+                <button
+                  className={
+                    values.status == false
+                      ? "py-2 px-3 rounded-md border border-blue-600"
+                      : "p-2 rounded-md border text-white bg-blue-600"
+                  }
+                  onClick={() => handlButtonClickStatus(values.name)}
+                >
+                  {values.name}
+                </button>
+              ))}
+            </div>
+
+            <div className="container">
+              <div className="grid grid-cols-4 gap-5 mt-5 mx-1">
+                {buttonRelatedData
+                  .filter((values) => values.category == currentButton)
+                  ?.map((values) => (
+                    <Card />
+                  ))}
+              </div>
+              <div className="flex mx-auto justify-center mt-3">
+                <button className="text-white text-lg bg-blue-600 rounded-sm p-2 w-100">
+                  Explore All Service
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
